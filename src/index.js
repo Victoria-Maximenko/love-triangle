b24f3c6785402b175a966812786bb898d3fbb842
Victoria <victoria.maximenko13@gmail.com>
2019-02-28 13:29:13 +0300

    style: improve code view

--- src/index.js
+++ src/index.js
@@ -2,25 +2,24 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
- 
+module.exports = function getLoveTrianglesCount(preferences = []){
   var result=0;
   var count=0;
-	var newArray=[];
-	newArray[0]='';
-	for(var i=0; i<preferences.length; i++){
-		newArray.push(preferences[i]);
-	}
-	for(var i=1; i<newArray.length; i++){
-      var first=newArray[i];
-      var second=newArray[first];
-      var third=newArray[second];
-      if(third===i){
-         result=result+1;
-      }
-   }
-   count=Math.floor(result/3)
-   return count;
+  var newArray=[];
+  newArray[0]='';
+  for(var i=0; i<preferences.length; i++){
+    newArray.push(preferences[i]);  
+  }
+  for(var i=1; i<newArray.length; i++){
+    var first=newArray[i];
+    var second=newArray[first];
+    var third=newArray[second];
+    if(third===i){
+      result=result+1;
+    }
+  }
+  count=Math.floor(result/3)
+  return count;
 };
 
 
